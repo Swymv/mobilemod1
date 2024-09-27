b7a130037b97f95a5ca8faa1d7d358ed6d326347
fito <FitoDiansyah@gmail.com>
2024-09-27 20:02:33 +0700

    Initial commit

---
+++ lib/Presentation/pages/home/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home Page Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Selamat datang',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Aksi ketika tombol ditekan
+                print('Button Pressed!');
+              },
+              child: Text('Masuk'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
